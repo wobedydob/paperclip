4c78ea5aa7268db77d0c24cf7f368528ca771de8
wuppo <wobjelsm@gmail.com>
2025-05-19 10:06:55 +0200

    fix: Config get() AND set() method

--- classes/Config.php
+++ classes/Config.php
@@ -25,29 +25,48 @@ class Config
     public function get(string $key, mixed $default = null): mixed
     {
         $keys = explode('.', $key);
-        $config = $this->config;
-        foreach ($keys as $key) {
-            if (!isset($config[$key])) {
+
+        // check if the config is an array and has a first element
+        $config = is_array($this->config) && isset($this->config[0])
+            ? $this->config[0]
+            : $this->config;
+
+        foreach ($keys as $segment) {
+            if (is_array($config) && array_key_exists($segment, $config)) {
+                $config = $config[$segment];
+            } else {
                 return $default;
             }
-            $config = $config[$key];
         }
+
         return $config;
     }
 
-    public function set(string $key, mixed $value): self
+    public function set(string $key, mixed $value): void
     {
         $keys = explode('.', $key);
-        $config = &$this->config;
-        foreach ($keys as $key) {
-            if (!isset($config[$key])) {
-                $config[$key] = [];
-            }
-            $config = &$config[$key];
+
+        // check if the config is an array and has a first element
+        if (is_array($this->config) && isset($this->config[0])) {
+            $config = &$this->config[0];
+        } else {
+            $config = &$this->config;
         }
-        $config = $value;
-        return $this;
+
+        foreach ($keys as $i => $segment) {
+            if ($i === count($keys) - 1) {
+                // set the value on the last segment
+                $config[$segment] = $value;
+            } else {
+                // automatically create intermediate arrays
+                if (!isset($config[$segment]) || !is_array($config[$segment])) {
+                    $config[$segment] = [];
+                }
+                $config = &$config[$segment];
+            }
+        }
     }
+
 
     public function all(): array
     {
